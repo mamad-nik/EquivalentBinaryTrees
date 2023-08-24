5d811e8558809188226ec1e3de2e4b8b64618257
mamad <khoshkhab11@gmail.com>
2023-08-25 01:20:31 +0330

    final

--- main.go
+++ main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"fmt"
+	"sort"
 	"sync"
 
+	"golang.org/x/exp/slices"
 	"golang.org/x/tour/tree"
 )
 
@@ -15,10 +17,8 @@ type counter struct {
 func Walk(t *tree.Tree, ch, quit chan int, c *counter, wg *sync.WaitGroup) {
 	if t != nil {
 		c.mu.Lock()
-		fmt.Printf("out: c.c: %v & ", c.c)
 		if c.c == 0 {
 			c.mu.Unlock()
-			fmt.Printf("in: c.c: %v\n", c.c)
 			quit <- 1
 			close(ch)
 			wg.Done()
@@ -30,10 +30,8 @@ func Walk(t *tree.Tree, ch, quit chan int, c *counter, wg *sync.WaitGroup) {
 
 		select {
 		case <-quit:
-			fmt.Printf("quit: i got it")
 			return
 		case ch <- t.Value:
-			fmt.Printf("t.Value: %v\n", t.Value)
 			wg.Add(2)
 			go Walk(t.Left, ch, quit, c, wg)
 			go Walk(t.Right, ch, quit, c, wg)
@@ -43,29 +41,45 @@ func Walk(t *tree.Tree, ch, quit chan int, c *counter, wg *sync.WaitGroup) {
 	wg.Done()
 }
 
-func Same(t1 *tree.Tree) {
+func array(t *tree.Tree, a *[]int, w *sync.WaitGroup) {
 	ch := make(chan int, 10)
 	quit := make(chan int)
 	c := &counter{c: 10}
 	var wg sync.WaitGroup
 	wg.Add(1)
-	go Walk(t1, ch, quit, c, &wg)
+	go Walk(t, ch, quit, c, &wg)
 	go func() {
 		defer wg.Done()
-		j := 0
 		for i := range ch {
-			fmt.Printf("%v, i: %v\n", j, i)
-			j++
+			*a = append(*a, i)
+
 		}
 	}()
 	wg.Wait()
+	w.Done()
 
+}
+func Same(t1, t2 *tree.Tree) bool {
+	var a, b []int
+	var wg sync.WaitGroup
+	wg.Add(2)
+	go array(t1, &a, &wg)
+	go array(t2, &b, &wg)
+	wg.Wait()
+	sort.Ints(a)
+	sort.Ints(b)
+
+	return slices.Equal(a, b)
 }
 
 func main() {
-	t0 := tree.New(1)
+	t1 := tree.New(1)
+	t2 := tree.New(1)
 
-	fmt.Println(t0)
-	Same(t0)
+	fmt.Printf("Same(t1, t2): %v\n", Same(t1, t2))
+	t3 := tree.New(1)
+	t4 := tree.New(2)
+
+	fmt.Printf("Same(t1, t2): %v\n", Same(t3, t4))
 
 }
